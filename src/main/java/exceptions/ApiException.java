5d7b44d6d2c6bef5d4af47ef3d92031379b1bd15
Nyirate <rwakaemma34@gmail.com>
2019-09-29 22:51:13 +0200

    add ApiException

--- src/main/java/exceptions/ApiException.java
+++ src/main/java/exceptions/ApiException.java
@@ -1,14 +1,14 @@
-//package exceptions;
-//
-//class ApiException extends RuntimeException {
-//    private final int statusCode;
-//
-//    public ApiException (int statusCode, String msg){
-//        super(msg); //see explanation below
-//        this.statusCode = statusCode;
-//    }
-//
-//    public int getStatusCode() {
-//        return statusCode;
-//    }
-//}
+package exceptions;
+
+class ApiException extends RuntimeException {
+    private final int statusCode;
+
+    public ApiException (int statusCode, String msg){
+        super(msg); //see explanation below
+        this.statusCode = statusCode;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+}
